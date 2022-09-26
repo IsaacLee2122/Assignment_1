2d9199fe9a12c0ae98d0141018bc9063a62711ec
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-09-26 18:36:13 -0500

    Update E1.java

--- E1.java
+++ E1.java
@@ -1,1 +1,12 @@
 //Isaac Lee, Exercise 1//
+public class Square {
+  public int side;
+  public int perimeter() {
+      int perimeter = side * 4;
+      return perimeter;
+  }
+  public int area () {
+      int area = side * side;
+      return area;
+    }
+}
