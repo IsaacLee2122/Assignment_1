5b50a77a49c0319d45c3bd993205efe390d401b5
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-09-26 18:42:12 -0500

    Update E2.java

--- E2.java
+++ E2.java
@@ -1,4 +1,6 @@
-//Isaac Lee, Exercise 2//
+//Isaac Lee, Exercise 2:Create a SquareTest class that contains a main() method that instantiates an object of type//
+//Square, with side of value 5. Then print the perimeter and the area of the object just created. //
+    
 public class SquareTest {
     public static void main(String args[]) {
         Square square = new Square ();
