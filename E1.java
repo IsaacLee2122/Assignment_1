8837f59909ca272128574a5860074b6f32bac83e
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-09-26 18:41:07 -0500

    Update E1.java

--- E1.java
+++ E1.java
@@ -1,4 +1,6 @@
-//Isaac Lee, Exercise 1//
+//Isaac Lee, Exercise 1 Create a Square class, which declares a side instance variable of type int. Then create a public method called perimeter() that returns the perimeter of the square, and a public area() method that returns the area of the square.//
+
+
 public class Square {
   public int side;
   public int perimeter() {
