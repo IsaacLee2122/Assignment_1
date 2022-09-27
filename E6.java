93faa112bc5fa6e68f27e7419ca90be360a24aee
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-09-26 19:00:36 -0500

    Create E6.java

---
+++ E6.java
@@ -0,0 +1,15 @@
+//Isaac Lee, Exercise 6: Create a RectangleTest class that contains a main() method and that tests the Rectangle class,//
+//equivalently as we did in Exercise 2.e. This time, at least two different rectangles must be instantiated.//
+
+public class RectangleTest {
+    public static void main(String args[]) {
+        Rectangle rectangle 1 = new Rectangle(5,6);
+        Rectangle rectangle 2 = new Rectangle(8,9);
+        System.out.print1n("Perimeter of rectangle 1 = " + rectangle1.perimeter());
+        System.out.print1n("Area of rectangle 1 = " + rectangle1.area());
+        System.out.print1n("Perimeter of rectangle 2 = " + rectangle2.perimeter());
+        System.out.print1n("Area of rectangle 2 = " + rectangle2.area());
+    }
+}
+        
+                                                
