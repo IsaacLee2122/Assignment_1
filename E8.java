a478cd4e32c4636a536a386d6e30ea602fca1cad
Isaac Lee <99067082+IsaacLee2122@users.noreply.github.com>
2022-09-26 19:10:42 -0500

    Create E8.java

---
+++ E8.java
@@ -0,0 +1,6 @@
+//Isaac Lee, Exercise 8: Starting from the solution of the previous exercise, create an additional method in
+//the Square and Rectangle classes, called getDetails(), which returns the same
+//string that was printed in the printDetails() method. After creating it, make sure that the
+//printDetails() method takes advantage of the getDetails() method so as not to duplicate
+//the code. Create a class called TestQuadrilaters that prints the details of a square and a rectangle.
+
